aa019850d4aed1851aa9cfdb23a9fdfa4f1603eb
Cong12212 <congglpro2547@gmail.com>
2025-12-15 20:31:47 +0700

    12/12/2025 - config deploy web

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,8 +2,7 @@ class AppConst {
   // API Base URL
   // static const apiBaseUrl = 'http://127.0.0.1:8000/api';
   // static const apiBaseUrl = 'https://bizz-connect.onrender.com/api';
-  static const apiBaseUrl = 'http://10.0.2.2:8000/api';
-
+  static const apiBaseUrl = 'https://biz-connect.online/backend/public/api';
   // Debug flags
   static const bool enableApiLogging = true;
   static const bool enableOcrLogging = true;
